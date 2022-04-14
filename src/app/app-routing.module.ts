8c72dec6f0b7d8b12c9045007e74f92e6c9379e2
souha azaiez <souha.azaiez@esprit.tn>
2022-04-14 17:56:35 +0200

    mon premier push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
+import { SouhaComponent } from './souha/souha.component';
+import { MonPremierComponent } from './mon-premier/mon-premier.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path:  'mon-premier', component: MonPremierComponent } ,
+{  path:  'souha', component: SouhaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/souha/souha.component.ts
+++ src/app/souha/souha.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-souha',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SouhaComponent implements OnInit {
 
-  constructor() { }
+  souhaage='26ans'
+  constructor(private router: Router) { 
+     }
 
   ngOnInit(): void {
   }
 
 }
+
+
+
